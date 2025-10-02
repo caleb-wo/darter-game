fa324a489700974848f0c594b91ee211dd768786
calebwolfe <caleb-wolfe@gmx.com>
2025-10-01 22:09:27 -0600

    Added RenderComponent.swift.

--- Darter/Architecture/GameComponents/RenderComponent.swift
+++ Darter/Architecture/GameComponents/RenderComponent.swift
@@ -12,6 +12,24 @@ import SpriteKit
 
 /// Primary renderig component for game
 /// entities.
-class RendComponent: GKComponent {
+class RenderComponent: GKComponent {
     let spriteNode: SKSpriteNode
+    
+    init(textureName: String, in size: CGSize){
+        let texture = SKTexture(imageNamed: textureName)
+        self.spriteNode = SKSpriteNode(texture: texture, size: size)
+        
+        self.spriteNode.userData = NSMutableDictionary()
+        self.spriteNode.userData?["component"] = self
+        
+        super.init()
+    }
+    
+    required init?(coder: NSCoder){
+        fatalError("init(coder:) has not been implemented.")
+    }
+    
+    func updateTexture(to newTextureName: String){
+        self.spriteNode.texture = SKTexture(imageNamed: newTextureName)
+    }
 }
