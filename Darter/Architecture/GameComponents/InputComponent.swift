16f632ca1c702228e4dd11365a2e2a20ff34eca6
calebwolfe <caleb-wolfe@gmx.com>
2025-10-02 15:19:20 -0600

    Created InputComponent empty class with TODOs.

---
+++ Darter/Architecture/GameComponents/InputComponent.swift
@@ -0,0 +1,14 @@
+//
+//  InputComponent.swift
+//  Darter
+//
+//  Created by Caleb Wolfe on 10/2/25.
+//
+
+import Foundation
+import GameplayKit
+
+class InputComponent: GKComponent {
+    // TODO: Finished this component and add it to the player entity.
+    // TODO: Create main scene and draw the player.
+}
