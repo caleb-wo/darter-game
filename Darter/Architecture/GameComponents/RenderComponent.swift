d5cbf3eed75d1c5d6ad5963b32367f1fdfe9c8a6
calebwolfe <caleb-wolfe@gmx.com>
2025-10-01 21:50:46 -0600

    Initial remote commit.

---
+++ Darter/Architecture/GameComponents/RenderComponent.swift
@@ -0,0 +1,17 @@
+//
+//  RenderComponent.swift
+//  Darter
+//
+//  Created by Caleb Wolfe on 10/1/25.
+//
+
+import Foundation
+import GameplayKit
+import SpriteKit
+
+
+/// Primary renderig component for game
+/// entities.
+class RendComponent: GKComponent {
+    let spriteNode: SKSpriteNode
+}
